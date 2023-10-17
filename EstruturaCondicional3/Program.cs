59d4d01d31d9d5ab34fa36a448217edc4dd7158d
Vitor Paiola <vitinho.souza1234@gmail.com>
2023-10-17 01:34:44 -0300

    Adicionar arquivos de projeto.

---
+++ EstruturaCondicional3/Program.cs
@@ -0,0 +1,9 @@
+﻿string[] vet = Console.ReadLine().Split(' ');
+int a = int.Parse(vet[0]);
+int b = int.Parse(vet[1]);
+
+if (a % b == 0 || b % a == 0) {
+    Console.WriteLine("Sao Multiplos");
+} else {
+    Console.WriteLine("Nao sao Multiplos");
+}
